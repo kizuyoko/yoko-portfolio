6f31a2e06456a0c4fc73831366154f1649f8eab6
kizuyoko <kizuyoko@gmail.com>
2025-08-05 14:36:13 +0200

    Debug for header shadow

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -5,7 +5,7 @@ export const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen bg-[var(--mainBackground)] defaultLayout">
       <Header />
-      <main className="flex flex-col flex-1 w-full max-w-screen-xl gap-6 mx-auto sm:flex-row-reverse">
+      <main className="flex flex-col flex-1 w-full sm:mt-1 max-w-screen-xl gap-6 mx-auto sm:flex-row-reverse">
         {children}
       </main>
       <Footer />
