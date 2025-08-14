e6aa4b5cb0f14b904ccd3d7eb4aebc652b4f28f8
kizuyoko <kizuyoko@gmail.com>
2025-08-14 13:21:12 +0200

    Data and Layout of project detail page

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -3,6 +3,11 @@ import { projects } from "@/data/projects";
 import { Title } from "@/components/ui/Title";
 import { notFound } from "next/navigation";
 import { slugify } from "@/utils/slugify";
+import Image from 'next/image';
+import { Button } from "@/components/ui/Button";
+import { SkillItem } from "@/components/ui/SkillItem";
+import { image } from "framer-motion/client";
+import { skills } from "@/data/skills";
 
 type ProjectPageProps = {
   params: Promise<{ projectId: string }>;
@@ -43,6 +48,38 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
   return (
     <section>
       <Title>{project.title}</Title>
+      <Image 
+        src={`/projects/${project.image}`}
+        width={640}
+        height={445}
+        alt={project.title}
+        priority
+        className="my-2 shadow sm:my-6"
+      />
+      <div className="flex flex-wrap gap-2 mt-2">
+        {project.techStack.map((tech) => (
+          <SkillItem key={tech} name={tech} />
+        ))}
+      </div>
+      <div className="flex items-center justify-center gap-2 my-6">
+        <Button 
+          href={project.demo}
+          ariaLabel={`Demo of ${project.title}`}
+        >Demo</Button>
+        <Button 
+          href={project.github}
+          ariaLabel={`Demo of ${project.title}`}
+        >GitHub</Button>
+      </div>
+      {
+        typeof project.description === "string" ?
+          (<p>{project.description}</p>)
+        : (
+          <>
+            {project.description}
+          </>
+        )  
+      }
     </section>
   );
 }
