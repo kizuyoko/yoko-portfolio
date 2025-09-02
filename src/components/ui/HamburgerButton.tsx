d4dc92b68c2885c9decccb43f7ebdbcc125d5c2d
kizuyoko <kizuyoko@gmail.com>
2025-09-02 13:22:38 +0200

    Dark mode

--- src/components/ui/HamburgerButton.tsx
+++ src/components/ui/HamburgerButton.tsx
@@ -14,9 +14,9 @@ export const HamburgerButton: React.FC<Props> = ({ navOpen, toggleNav }) => (
       className="flex flex-col justify-center items-center w-8 h-8 gap-1.5 sm:hidden"
       onClick={toggleNav}
     >
-      <span className={`block h-0.5 w-6 bg-black transition-all duration-300 ease-in-out ${navOpen ? "rotate-45 translate-y-2" : ""}`} />
-      <span className={`block h-0.5 w-6 bg-black transition-all duration-300 ease-in-out ${navOpen ? "opacity-0" : ""}`} />
-      <span className={`block h-0.5 w-6 bg-black transition-all duration-300 ease-in-out ${navOpen ? "-rotate-45 -translate-y-2" : ""}`} />
+      <span className={`block h-0.5 w-6 bg-[var(--color-text)] duration-300 ${navOpen ? "rotate-45 translate-y-2" : ""}`} />
+      <span className={`block h-0.5 w-6 bg-[var(--color-text)] duration-300 ${navOpen ? "opacity-0" : ""}`} />
+      <span className={`block h-0.5 w-6 bg-[var(--color-text)] duration-300 ${navOpen ? "-rotate-45 -translate-y-2" : ""}`} />
     </button>
   </div>
 );
